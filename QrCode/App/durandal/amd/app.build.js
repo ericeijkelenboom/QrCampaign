d897795ae31a5be2ab07966af8bc1307e249d32a
ericeijkelenboom <eric.eijkelenboom@gmail.com>
2013-03-11 09:53:45 +0100

    Include app-built.js in project file

---
+++ QrCode/App/durandal/amd/app.build.js
@@ -0,0 +1,54 @@
+{
+  "name": "durandal/amd/almond-custom",
+  "inlineText": true,
+  "stubModules": [
+    "durandal/amd/text"
+  ],
+  "paths": {
+    "text": "durandal/amd/text"
+  },
+  "baseUrl": "c:\\Users\\drexeeij\\Documents\\Visual Studio 2012\\Projects\\QrCode\\QrCode\\App",
+  "mainConfigFile": "c:\\Users\\drexeeij\\Documents\\Visual Studio 2012\\Projects\\QrCode\\QrCode\\App\\main.js",
+  "include": [
+    "main",
+    "durandal/app",
+    "durandal/composition",
+    "durandal/events",
+    "durandal/http",
+    "text!durandal/messageBox.html",
+    "durandal/messageBox",
+    "durandal/modalDialog",
+    "durandal/system",
+    "durandal/viewEngine",
+    "durandal/viewLocator",
+    "durandal/viewModel",
+    "durandal/viewModelBinder",
+    "durandal/widget",
+    "durandal/plugins/router",
+    "durandal/transitions/entrance",
+    "services/backend",
+    "services/localstore",
+    "services/logger",
+    "viewmodels/details",
+    "viewmodels/scan",
+    "viewmodels/shell",
+    "viewmodels/subscriptions",
+    "text!views/details.html",
+    "text!views/footer.html",
+    "text!views/nav.html",
+    "text!views/scan.html",
+    "text!views/shell.html",
+    "text!views/subscriptions.html"
+  ],
+  "exclude": [],
+  "keepBuildDir": true,
+  "optimize": "uglify2",
+  "out": "c:\\Users\\drexeeij\\Documents\\Visual Studio 2012\\Projects\\QrCode\\QrCode\\App\\main-built.js",
+  "pragmas": {
+    "build": true
+  },
+  "wrap": true,
+  "insertRequire": [
+    "main"
+  ]
+}
