f25f77861ddcec79ed117b35a966b85a5497783c
DREXEEIJ <eric.marius@cybercom.com>
2013-04-08 20:01:48 +0200

    New datacontext with lazy loading

--- QrCode/App/viewmodels/scan.js
+++ QrCode/App/viewmodels/scan.js
@@ -1,32 +1,27 @@
-﻿define(['services/logger', 'services/datacontext', 'services/localstore'],
-    function (logger, datacontext, localstore) {
-        var vm = {
-            activate: activate,
-            title: ko.observable('Scanning...'),
-            campaignDescription: ko.observable(''),
-            numberOfPoints: ko.observable(0)
-        };
+﻿define(['services/logger', 'services/backend', 'services/localstore'], function (logger, backend, localstore) {
+    var vm = {
+        activate: activate,
+        title: ko.observable('Scanning...'),
+        campaignDescription: ko.observable(''),
+        numberOfPoints: ko.observable(0)
+    };
 
-        return vm;
+    return vm;
 
-        //#region Internal Methods
-        function activate(context) {
-            logger.log('Scan View Activated', null, 'scan', true);
+    //#region Internal Methods
+    function activate(context) {
+        logger.log('Scan View Activated', null, 'scan', true);
 
-            var customerId = localstore.getCustomerId();
+        var customerId = localstore.getCustomerId();
 
-            return datacontext.scan(customerId, context.campaignId, context.qrId)
-                .then(scanSucceeded)
-                .then(datacontext.getSubscriptions(customerId, null, true)); // refresh subscription data
-        }
-    
-        function scanSucceeded(data) {
-            localstore.setCustomerId(data.CustomerId);
+        return backend.scan(customerId, context.campaignId, context.qrId)
+            .then(function (data) {
+                localstore.setCustomerId(data.CustomerId);
 
-            vm.title('Congratulations!');
-            vm.numberOfPoints(data.Subscription.NumberOfPoints);
-            vm.campaignDescription(data.CampaignDescription);
-        }
-    
-        //#endregion
+                vm.title('Congratulations!');
+                vm.numberOfPoints(data.Subscription.NumberOfPoints);
+                vm.campaignDescription(data.CampaignDescription);
+            });
+    }
+    //#endregion
 });
--- QrCode/App/viewmodels/subscriptions.js
+++ QrCode/App/viewmodels/subscriptions.js
@@ -1,4 +1,4 @@
-﻿define(['services/logger', 'services/datacontext', 'services/localstore'], function (logger, datacontext, localstore) {
+﻿define(['services/logger', 'services/backend', 'services/localstore'], function (logger, backend, localstore) {
     var vm = {
         title: 'My subscriptions',
         subscriptions: ko.observableArray([]),
@@ -19,11 +19,11 @@
     
     function refresh() {
         // Fetch subscriptions
-        return datacontext.getSubscriptions(localstore.getCustomerId(), vm.subscriptions);
+        return backend.getSubscriptions(localstore.getCustomerId(), vm.subscriptions);
     }
     
     function isRedeemAvailable(subscription) {
-        return subscription.numberOfPoints() >= subscription.campaign().numberOfPointsBeforeRedeem();
+        return subscription.NumberOfPoints() >= subscription.Campaign().NumberOfPointsBeforeRedeem();
     }
 
     //#endregion
